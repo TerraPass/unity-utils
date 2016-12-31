3d07589756388b85f68139b07d65c67f082499a8
Taras Denysenko <terrapass93@gmail.com>
2016-12-31 12:38:42 +0200

    Changed AssemblyCulture for Terrapass.Debug to an empty string.

--- Debug/Properties/AssemblyInfo.cs
+++ Debug/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Taras Denysenko")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
